84d1b8125ebffbbbfa9380951021acc1f42c7737
Gabriel <gabriel.slmendoza@gmail.com>
2024-07-27 14:09:12 -0500

    add variables

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Hello world!");
+
+        // Variables
+        String name = "John Doe";
+        char firstNameLetter = 'J';
+        int age = 26;
+        boolean isMale = true;
+        double cash = 10.5;
+
+        String genre = isMale ? "Male" : "Female";
+        String pronoun = isMale ? "He" : "She";
+        String determiner = isMale ? "His" : "Her";
+
+        System.out.println(pronoun +" is "+ name);
+        System.out.println(determiner +" first name letter is "+ firstNameLetter);
+        System.out.println(pronoun +" is "+ age);
+        System.out.println(determiner +" genre is " + genre);
+        System.out.println(pronoun +" has "+ cash + " in " + determiner.toLowerCase() + " pocket");
+
     }
 
 }
